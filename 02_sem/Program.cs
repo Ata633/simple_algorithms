4905a0a1750927fb79ca894d1cdfb7ad7fb150fe
Atajan <atajanjumayev1991@gmail.com>
2024-03-24 14:56:40 +0900

    Add algorithm

--- 02_sem/Program.cs
+++ 02_sem/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на 
+// вход трёхзначное число и удаляет вторую 
+// цифру этого числа.
+// Примеры
+// a = 256 => 26 
+// a = 891 => 81
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int first_num = num / 100;
+int last_num = num % 10;
+
+int result_num = (first_num * 10) + last_num;
+Console.WriteLine(result_num);
